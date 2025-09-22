a13c7afd56fce606128fd5c408a41ca7b2490f94
shawky <mastershawky2017@gmail.com>
2025-09-22 21:58:17 +0300

    remove unused javascript code

--- script.js
+++ script.js
@@ -223,16 +223,3 @@ menuOverlay.addEventListener("click", () => {
     navToggle();
   }
 });
-
-// Add smooth scrolling to all links
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute("href"));
-    if (target) {
-      target.scrollIntoView({
-        behavior: "smooth",
-      });
-    }
-  });
-});
